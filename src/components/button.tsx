e4e7c290ee7e404f17eb371edbef1466ea02b345
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-03-04 22:40:24 +0500

    first commit

---
+++ src/components/button.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export function Button(): JSX.Element {
+    const [count, setCount] = useState<number>(1);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <>
+            <div className="h-screen  flex items-center justify-center">
+                <button className="h-10 bg-white " onClick={handleClick}>
+                    click {count}
+                </button>
+            </div>
+        </>
+    );
+}
